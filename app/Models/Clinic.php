9dbbfd3bb6409611b421e1c787f8136afbc6308c
Subiramaniam Pirashanth <79297844+Shanth05@users.noreply.github.com>
2025-07-04 15:05:20 +0530

    Update Clinic.php

--- app/Models/Clinic.php
+++ app/Models/Clinic.php
@@ -7,10 +7,22 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use App\Models\User;
 use App\Models\Patient;
+use Illuminate\Support\Str;
 use App\Models\Appointment;
 
 class Clinic extends Model
 {
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($clinic) {
+            if (empty($clinic->slug)) {
+                $clinic->slug = Str::slug($clinic->name);
+            }
+        });
+    }
+    
     public function users():BelongsToMany
     {
         return $this->belongsToMany(User::class, 'clinic_user');
